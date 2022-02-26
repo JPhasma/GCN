6017177de7135fc03db249014b47fd32671b3f70
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-26 12:19:12 +0000

    added context api

---
+++ src/contexts/DataContext.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, createContext } from 'react';
+// import axios from 'axios';
+
+// import the sample data
+import ResultsSet from '../data/data.json';
+
+export const DataContext = createContext();
+
+export const DataContextProvider = ({ children }) => {
+  // set up context api states to share between context components
+  const [videoData, setVideoData] = useState([]);
+
+  // JSON video data
+  // useEffect(() => {
+  //   axios
+  //     // issue with CORS policy so pulled in localy for now
+  //     // .get('https://www.globalcyclingnetwork.com/api/devtask')
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.error('Error retrieving the JSON data from API');
+  //     });
+  // });
+
+  useEffect(() => {
+    setVideoData(ResultsSet);
+  }, [videoData]);
+
+  // return DataContext
+  return (
+    <DataContext.Provider
+      value={{
+        videoData,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,9 +1,13 @@
+import { DataContextProvider } from '../contexts/DataContext';
+
 import { Header } from '../components/Header';
 
 export const Layout = () => {
   return (
     <div>
-      <Header />
+      <DataContextProvider>
+        <Header />
+      </DataContextProvider>
     </div>
   );
 };
