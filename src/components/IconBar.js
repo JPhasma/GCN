31657203409f83bac12601a53efd18635c17f1ef
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-27 17:38:06 +0000

    split our iconbar and navlist into components

---
+++ src/components/IconBar.js
@@ -0,0 +1,60 @@
+import './IconBar.scss';
+
+import { FaHome as HomeIcon } from 'react-icons/fa';
+import facebook from '../images/IconCrc_Facebook.png';
+import twitter from '../images/IconCrc_Twitter.png';
+import instagram from '../images/IconCrc_Instagram.png';
+import { FaSearch as SearchIcon } from 'react-icons/fa';
+import { FiMenu as MenuIcon } from 'react-icons/fi';
+
+export const IconBar = () => {
+  return (
+    <div className='iconbar'>
+      <ul className='iconbar_links'>
+        <li>
+          <a href='/'>
+            <HomeIcon alt='Home' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://facebook.com/globalcyclingnetwork'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={facebook} alt='Link to GCN Facebook' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://twitter.com/gcntweet'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={twitter} alt='Link to GCN Twitter' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://instagram.com/globalcyclingnetwork/?hl=en'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={instagram} alt='Link to GCN Instagram' />
+          </a>
+        </li>
+        {/* <!-- not included google+ as its gone --> */}
+        <li>
+          <a href='/Search'>
+            <SearchIcon alt='Search' />
+          </a>
+        </li>
+        <li>
+          <a href='/Menu'>
+            <MenuIcon alt='Menu Toggle Button' />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,124 +3,14 @@ import { DataContext } from '../contexts/DataContext';
 
 import './Nav.scss';
 
-import { FaHome as HomeIcon } from 'react-icons/fa';
-import facebook from '../images/IconCrc_Facebook.png';
-import twitter from '../images/IconCrc_Twitter.png';
-import instagram from '../images/IconCrc_Instagram.png';
-import { FaSearch as SearchIcon } from 'react-icons/fa';
-import { FiMenu as MenuIcon } from 'react-icons/fi';
+import { IconBar } from './IconBar';
+import { NavList } from './NavList';
 
 export const Nav = () => {
   return (
     <nav>
-      <div className='iconbar'>
-        <ul className='iconbar_social'>
-          <li>
-            <a href='/'>
-              <HomeIcon alt='Home' />
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://facebook.com/globalcyclingnetwork'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <img src={facebook} alt='Link to GCN Facebook' />
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://twitter.com/gcntweet'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <img src={twitter} alt='Link to GCN Twitter' />
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://instagram.com/globalcyclingnetwork/?hl=en'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <img src={instagram} alt='Link to GCN Instagram' />
-            </a>
-          </li>
-          <li>
-            <a href='/Search'>
-              <SearchIcon alt='Search' />
-            </a>
-          </li>
-          <li>
-            <a href='/Menu'>
-              <MenuIcon alt='Menu Toggle Button' />
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <ul className='container' className='main_nav'>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/category/gcn-show'>GCN Show</a>
-        </li>
-        <li>
-          <a href='/category/how-to'>How To</a>
-        </li>
-        <li>
-          <a href='/category/maintenance-mondays'>Maintenance</a>
-        </li>
-        <li>
-          <a href='/category/ask-gcn'>Ask GCN</a>
-        </li>
-        <li>
-          <a href='/category/train-with-gcn'>Training </a>
-        </li>
-        <li>
-          <a href='/category/features'>Features</a>
-        </li>
-        <li>
-          <a href='/category/top-10s'>Top 10s</a>
-        </li>
-        <li>
-          <a href='/category/gcn-racing'>GCN Racing</a>
-        </li>
-        <li>
-          <a href='/category/gcn-tech'>GCN Tech</a>
-        </li>
-        <li>
-          <a href='/presenters'>Presenters</a>
-          <ul>
-            <li>
-              <a href='/presenters/conor-dunne'>Conor Dunne</a>
-            </li>
-            <li>
-              <a href='/presenters/daniel-lloyd'>Daniel Lloyd</a>
-            </li>
-            <li>
-              <a href='/presenters/james-lw'>James Lowsley-Williams</a>
-            </li>
-            <li>
-              <a href='/presenters/jon-cannings'>Jon Cannings</a>
-            </li>
-            <li>
-              <a href='/presenters/manon-lloyd'>Manon Lloyd</a>
-            </li>
-            <li>
-              <a href='/presenters/oliver-bridgewood'>Oliver Bridgewood</a>
-            </li>
-            <li>
-              <a href='/presenters/simon-richardson'>Simon Richardson</a>
-            </li>
-            <li>
-              <a href='/presenters/tom-last'>Tom Last</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <IconBar />
+      <NavList />
     </nav>
   );
 };
