2fe76accde3b81f2842d6ba7c00c5736e6467799
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-27 19:30:03 +0000

    basic menu grid css layout

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,102 +1,186 @@
+import { useContext } from 'react';
+import { DataContext } from '../contexts/DataContext';
+
 import './NavList.scss';
 
 import { Link } from 'react-router-dom';
 
 export const NavList = () => {
+  const { setMenuStatus } = useContext(DataContext);
+
+  const closeMenu = (e) => {
+    setMenuStatus(false);
+  };
   return (
     <ul className='container' className='main_nav'>
       <li>
-        <Link className='anchor_item' to='/'>
+        <Link onClick={closeMenu} className='anchor_item' to='/'>
           Home
         </Link>
       </li>
       <li>
-        <Link className='anchor_item' to='/category/gcn-show'>
-          GCN Show
+        <Link onClick={closeMenu} className='anchor_item' to='/'>
+          Content
         </Link>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/gcn-show'
+            >
+              GCN Show
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/how-to'
+            >
+              How To
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/maintenance-mondays'
+            >
+              Maintenance
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/ask-gcn'
+            >
+              Ask GCN
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/train-with-gcn'
+            >
+              Training{' '}
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/features'
+            >
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/top-10s'
+            >
+              Top 10s
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/gcn-racing'
+            >
+              GCN Racing
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/category/gcn-tech'
+            >
+              GCN Tech
+            </Link>
+          </li>
+        </ul>
       </li>
+
       <li>
-        <Link className='anchor_item' to='/category/how-to'>
-          How To
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/maintenance-mondays'>
-          Maintenance
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/ask-gcn'>
-          Ask GCN
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/train-with-gcn'>
-          Training{' '}
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/features'>
-          Features
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/top-10s'>
-          Top 10s
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/gcn-racing'>
-          GCN Racing
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/category/gcn-tech'>
-          GCN Tech
-        </Link>
-      </li>
-      <li>
-        <Link className='anchor_item' to='/presenters'>
+        <Link onClick={closeMenu} className='anchor_item' to='/presenters'>
           Presenters
         </Link>
         <ul>
           <li>
-            <Link className='anchor_item' to='/presenters/conor-dunne'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/conor-dunne'
+            >
               Conor Dunne
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/daniel-lloyd'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/daniel-lloyd'
+            >
               Daniel Lloyd
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/james-lw'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/james-lw'
+            >
               James Lowsley-Williams
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/jon-cannings'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/jon-cannings'
+            >
               Jon Cannings
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/manon-lloyd'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/manon-lloyd'
+            >
               Manon Lloyd
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/oliver-bridgewood'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/oliver-bridgewood'
+            >
               Oliver Bridgewood
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/simon-richardson'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/simon-richardson'
+            >
               Simon Richardson
             </Link>
           </li>
           <li>
-            <Link className='anchor_item' to='/presenters/tom-last'>
+            <Link
+              onClick={closeMenu}
+              className='anchor_item'
+              to='/presenters/tom-last'
+            >
               Tom Last
             </Link>
           </li>
