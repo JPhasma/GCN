9d169768cdab6f387fcd98de68517d05924336eb
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-27 23:21:53 +0000

    added footer screengrab for mobile

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import FooterImg from '../images/placement_images/Footer_screenshot_mb.png';
+
+import './Footer.scss';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <img src={FooterImg} alt='UX rapid layout element' />
+    </footer>
+  );
+};
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -4,6 +4,7 @@ import { Header } from '../components/Header';
 import { VideoList } from '../components/VideoList';
 import { VideoPage } from '../components/VideoPage';
 import { Page } from '../components/Page';
+import { Footer } from '../components/Footer';
 
 export const Layout = () => {
   return (
@@ -14,6 +15,7 @@ export const Layout = () => {
         <Route path='/video/:slug' element={<VideoPage />}></Route>
         <Route path='*' element={<Page />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 };
