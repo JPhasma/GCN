facb375eea2a5973bb3ddf5ad41f643f1e083bd3
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-28 03:28:31 +0000

    removed the let_scroll css - no longer needed

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -8,22 +8,9 @@ import { Footer } from '../components/Footer';
 
 import './Layout.scss';
 
-// Layout issue:
-// if small device is viewing in landscape,
-// nav dropdown can't be scrolled
-// determine viewport and if small device
-// add layout class to allow scrolling
-const vh = Math.max(
-  document.documentElement.clientHeight || 0,
-  window.innerHeight || 0
-);
-const mobileNavHeight = 700;
-console.log(vh);
-if (vh < mobileNavHeight) console.log('LET SCROLL');
-
 export const Layout = () => {
   return (
-    <div className={vh < mobileNavHeight ? 'let_scroll' : 'fix_tablet'}>
+    <div>
       <Header />
       <main className='main_content'>
         <Routes>
